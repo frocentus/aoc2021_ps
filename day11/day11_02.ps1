cf8bbdb40836af0286be9b14961a859890198fcc
Harald Reisinger <harald@reisinger.ws>
2021-12-11 20:09:42 +0100

    Solutions for day 11

---
+++ day11/day11_02.ps1
@@ -0,0 +1,108 @@
+$data = Get-Content -Path "$($PSScriptRoot)/input.txt"
+
+$global:xdim = $data[0].ToCharArray().Length
+$global:ydim = $data.Length
+$global:octopuses = New-Object Octopus[][] $xdim, $ydim
+$global:flashes = 0
+
+
+class Octopus {
+    [int]$x
+    [int]$y
+    [int]$level
+    [bool]$has_flashed
+
+    Octopus($x, $y, $level) {
+        $this.x = $x
+        $this.y = $y
+        $this.level = $level
+    }
+
+    IncreaseLevel() {
+        if ($this.has_flashed -eq $false) {
+            $this.level = $this.level + 1
+            if ($this.level -gt 9) {
+                $global:flashes += 1
+                $this.has_flashed = $true
+                $this.level = 0
+                $x1 = -1
+                $y1 = -1
+                    $y1 = $this.y-1; 
+                    if ($y1 -ge 0) {
+                        $x1 = $this.x-1; 
+                        if ($x1 -ge 0) { 
+                            $global:octopuses[$y1][$x1].IncreaseLevel()
+                        }
+                        $x1 = $this.x;   
+                        $global:octopuses[$y1][$x1].IncreaseLevel()
+                        $x1 = $this.x+1; 
+                        if ($x1 -lt $global:xdim) { 
+                            $global:octopuses[$y1][$x1].IncreaseLevel()
+                        }
+                    }
+
+                    $y1 = $this.y; 
+                    $x1 = $this.x-1; 
+                    if ($x1 -ge 0) { 
+                        $global:octopuses[$y1][$x1].IncreaseLevel()
+                    }
+                    $x1 = $this.x+1; 
+                    if ($x1 -lt $global:xdim) { 
+                        $global:octopuses[$y1][$x1].IncreaseLevel()
+                    }
+                    
+                    $y1 = $this.y+1; 
+                    if ($y1 -lt $global:ydim) {
+                        $x1 = $this.x-1; 
+                        if ($x1 -ge 0) { 
+                            $global:octopuses[$y1][$x1].IncreaseLevel()
+                        }                    
+                        $x1 = $this.x;   
+                        $global:octopuses[$y1][$x1].IncreaseLevel()
+
+                        $x1 = $this.x+1; 
+                        if ($x1 -lt $global:xdim) { 
+                            $global:octopuses[$y1][$x1].IncreaseLevel()
+                        }  
+                    }
+            }
+        }
+    }
+}
+
+
+$y = 0
+foreach($line in $data) {
+    $chars = $line.ToCharArray()
+    for($x=0; $x -lt $xdim; $x++) {
+        $global:octopuses[$y][$x] = [Octopus]::new($x, $y, [int]"$($chars[$x])")
+    }
+    $y++
+}
+
+for($r=1; $r -le 1000; $r++) {
+    $global:flashes = 0
+    Clear-Host
+    for($y=0; $y -lt $ydim; $y++) {
+        for($x=0; $x -lt $xdim; $x++) {
+            $octopuses[$y][$x].has_flashed = $false
+        }
+    }
+    for($y=0; $y -lt $ydim; $y++) {
+        for($x=0; $x -lt $xdim; $x++) {
+            $op = $octopuses[$y][$x]
+            $op.IncreaseLevel()
+            if ($op.level -eq 0) {
+                Write-Host "$($op.level)" -NoNewline -ForegroundColor Yellow
+            } else {
+                Write-Host "$($op.level)" -NoNewline -ForegroundColor Gray
+            }
+        }
+        Write-Host ""
+    }
+    if ($global:flashes -eq $xdim * $ydim) {
+        Write-Host "All Flashed in Round $r"
+        break
+    }
+}
+
